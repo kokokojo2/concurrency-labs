4566282f7da34a7f80a5f358476742e048215e35
kokokojo2 <foxgunplay@gmail.com>
2022-06-11 10:31:03 +0300

    GameServer and GameClient were designed, login procedure was implemented

--- lab5/client.cpp
+++ lab5/client.cpp
@@ -1,15 +1,109 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 #include "socket.h"
+#include "constants.h"
+#include "protocol.cpp"
 
-# define SERVER_ADDRESS "127.0.0.1"
-#define SERVER_PORT 5544
+class GameClient : public Logger {
+private:
+    ClientSocket socket;
+    std::string prefix = "GameClient";
 
+    void writeCommand(TicTacToeCommand ticTacToeCommand) {
+        this->socket.writeMessage(commandToString(ticTacToeCommand));
+    }
+
+    std::string waitForMessage() {
+        while (true) {
+            std::string message = this->socket.readMessage();
+            if (!message.empty()) return message;
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+    }
+
+    TicTacToeMessage waitForValidMessage() {
+        while (true) {
+            std::string message = this->waitForMessage();
+            TicTacToeMessage parsedMessage = parseResponse(message);
+            if (!parsedMessage.valid) {
+                this->print("Got invalid message. This is likely to be the problem with server.", false);
+                printMessage(parsedMessage);
+                continue;
+            }
+            return parsedMessage;
+        }
+    }
+
+    // TODO: make chooseCommandDialog instead
+    int optionDialog(std::vector<std::string> options) {
+        this->print("Showing choose option dialog:", false);
+        int option;
+        while (true) {
+            std::cout << "Choose one of the following options:" << std::endl;
+            for(int i = 0; i < options.size(); i++) {
+                std::cout << std::to_string(i) << ") " << options[i] << std::endl;
+            }
+
+            std::cin >> option;
+            if (option < options.size()) return option;
+        }
+    }
+
+    user connectToGame() {
+        TicTacToeCommand connectToGameCommand;
+        connectToGameCommand.command = command.connectToGame;
+        this->writeCommand(connectToGameCommand);
+        TicTacToeMessage responseMessage = this->waitForValidMessage();
+        user currentUser{};
+
+         // user is connected to game as a player
+        if (responseMessage.status == status.accepted) {
+            currentUser.type = user_type.player;
+         // the server is busy
+        } else {
+            this->print("The server has no free slots", false);
+            // choose between watch game or leave
+            std::vector<std::string> options{"watch current game.", "disconnect."};
+            // returns the index of chosen option
+            int option = this->optionDialog(options);
+
+            TicTacToeCommand optionCommand;
+            optionCommand.command = option == 0 ? command.watchGame : command.disconnect;
+
+            this->writeCommand(optionCommand);
+            auto response = this->waitForValidMessage();
+            if (response.status == status.accepted) {
+                currentUser.type = optionCommand.command == command.watchGame ? user_type.spectator : user_type.exited;
+            }
+        }
+
+        return currentUser;
+    }
+public:
+    explicit GameClient(int port) : socket(port) {
+        this->prefix = "Client";
+
+    };
+    void run() {
+        socket.connect(SERVER_ADDRESS, SERVER_PORT);
+        user currentUser = this->connectToGame();
+        if (currentUser.type == user_type.exited) return;
+
+        this->print("You have successfully logged in: " + getUserRepr(currentUser), false);
+        if (currentUser.type == user_type.player) {
+            // TODO: start game, handle player
+        }
+        if (currentUser.type == user_type.spectator) {
+            // TODO: handle spectator
+        }
+    };
+};
 int main() {
-    auto clientSock = Socket(5555);
-    clientSock.writeToServer(SERVER_ADDRESS, SERVER_PORT, "Hello!");
-    // block
-    std::string receivedMessage = clientSock.getResponse();
-
-    std::cout << receivedMessage << std::endl;
+    std::cout << "Enter the port which the client should use: ";
+    int port;
+    std::cin >> port;
+    auto client = GameClient(port);
+    client.run();
     return 0;
 }
--- lab5/server.cpp
+++ lab5/server.cpp
@@ -1,13 +1,135 @@
 #include <iostream>
+#include <thread>
+#include <mutex>
+#include "protocol.cpp"
 #include "socket.h"
+#include "constants.h"
 
+std::mutex resourcesMutex;
+
+// TODO: one game, two clients
+class GameServer : public Logger {
+private:
+    ServerSocket socket;
+    int playerOneConnectionDesc = -1;
+    int playerTwoConnectionDesc = -1;
+
+    void setPlayerOneConnection(int connectionDesc) {
+        this->playerOneConnectionDesc = connectionDesc;
+    }
+
+    void setPlayerTwoConnection(int connectionDesc) {
+        this->playerTwoConnectionDesc = connectionDesc;
+    }
+
+    int setPlayer(int connectionDesc) {
+        std::lock_guard<std::mutex> lock(resourcesMutex);
+        if (this->playerOneConnectionDesc == -1) {
+            this->setPlayerOneConnection(connectionDesc);
+            return 1;
+        }
+        if (this->playerTwoConnectionDesc == -1) {
+            this->setPlayerTwoConnection(connectionDesc);
+            return 2;
+        }
+        return -1;
+    }
+
+    std::string waitForMessage(int connectionDesc) {
+        while (true) {
+            std::string message = this->socket.getMessage(connectionDesc);
+            if (!message.empty()) return message;
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+    }
+    TicTacToeCommand waitForValidCommand(int connectionDesc) {
+        while (true) {
+            std::string message = this->waitForMessage(connectionDesc);
+            TicTacToeCommand parsedCommand = parseRequest(message);
+            if (parsedCommand.valid) return parsedCommand;
+
+            this->print("Got invalid command. This is likely to be the problem with client.", false);
+            printCommand(parsedCommand);
+
+            TicTacToeMessage messageInvalidCommand;
+            messageInvalidCommand.status = status.previousCommandInvalid;
+            this->sendMessage(connectionDesc, messageInvalidCommand);
+        }
+    }
+
+    void sendMessage(int connectionDesc, const TicTacToeMessage&  message) {
+        socket.sendMessage(connectionDesc, messageToString(message));
+    }
+
+    user handleNewUser(int connectionDesc) {
+        auto parsedCommand = this->waitForValidCommand(connectionDesc);
+        this->print(
+                "New client has connected and wants to " + getReprCommand(parsedCommand.command) + ".",
+                true);
+
+        user currentUser{};
+        currentUser.type = user_type.noType;
+        TicTacToeMessage message;
+
+        if (parsedCommand.command == command.watchGame) {
+            currentUser.type = user_type.spectator;
+            message.status = status.accepted;
+        }
+        if (parsedCommand.command == command.disconnect) {
+            currentUser.type = user_type.exited;
+            message.status = status.accepted;
+        }
+        if (parsedCommand.command == command.connectToGame) {
+            // trying to set user if slots are not busy
+            currentUser.id = setPlayer(connectionDesc);
+            // returning message with error if there are no free slot
+            if (currentUser.id == -1) {
+                message.status = status.error;
+                this->print("There is no free slot for this user.", true);
+            } else {
+                // user was set successfully
+                currentUser.type = user_type.player;
+                message.status = status.accepted;
+            }
+        }
+
+        this->sendMessage(connectionDesc, message);
+        return currentUser;
+    }
+
+    void serveClient(int connectionDesc) {
+        user currentUser{};
+        do {
+            currentUser = this->handleNewUser(connectionDesc);
+        } while (currentUser.type == user_type.noType);
+        this->print("New user was logged in: " + getUserRepr(currentUser), true);
+
+        if (currentUser.type == user_type.exited) return;
+        if (currentUser.type == user_type.player) {
+            // TODO: start game, handle player
+        }
+        if (currentUser.type == user_type.spectator) {
+            // TODO: handle spectator
+        }
+    }
+public:
+    GameServer() : socket(SERVER_PORT) {
+        this->prefix = "Server";
+    };
+    void run() {
+        socket.binAndListen();
+        while (true) {
+            int connectionDesc = socket.waitForConnection();
+            std::thread t1(&GameServer::serveClient, this, connectionDesc);
+            t1.detach();
+            // TODO: create procedure thread that works with client until it disconnect
+        }
+    };
+};
+
+// TODO: process every connection in different thread
 int main() {
-    auto serverSock = Socket(5544);
-    serverSock.binAndListen();
-    // block
-    std::string receivedMessage = serverSock.waitForMessage();
-
-    std::cout << receivedMessage << std::endl;
-    serverSock.reply("World!");
+    auto server = GameServer();
+    server.run();
     return 0;
 }
